f0f2de267bf3e6b63feca5aaf6b81d2826344376
Christopher Jackson <himself@cj-jackson.com>
2012-05-04 23:30:33 +0100

    Implemented support for YouTube Playlist!

--- mediaembedder/services/youtube.py
+++ mediaembedder/services/youtube.py
@@ -8,16 +8,14 @@ def youtube(self):
         try:
             width = int(self.data['meta']['og:video:width'])
         except:
-            width = 398
-
+            width = 640
     if self.height:
         height = self.height
     else:
         try:
             height = int(self.data['meta']['og:video:height'])
         except:
-            height = 224
-
+            height = 368
     return self.render('youtube.html', {
         'id': id, 'width': width, 'height': height
     })
@@ -33,4 +31,23 @@ services.append({
 services.append({
         're': '^http(s?)://youtu.be/(?P<youtube_id>[a-zA-Z0-9-_]+)',
         'func': youtube,
+    })
+
+def youtube_playlist(self):
+    id = self.match.group('youtube_playlist_id')
+    if self.width:
+        width = self.width
+    else:
+        width = 640
+    if self.height:
+        height = self.height
+    else:
+        height = 368
+    return self.render('youtube_playlist.html', {
+        'id': id, 'width': width, 'height': height
+    })
+
+services.append({
+        're': '^http(s?)://([a-z]*).youtube.com/playlist?(.*)list=(?P<youtube_playlist_id>[a-zA-Z0-9-_]+)',
+        'func': youtube_playlist,
     })
