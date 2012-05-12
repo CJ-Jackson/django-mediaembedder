4258194766b225ad6707217b77645beddc220dd0
Christopher Jackson <himself@cj-jackson.com>
2012-05-12 11:40:58 +0100

    Added support for revision3 broadcast!

--- mediaembedder/services/revision3.py
+++ mediaembedder/services/revision3.py
@@ -25,3 +25,29 @@ services.append({
         're': '^http(s?)://(.*)revision3.com/(?P<revision3_group>[a-zA-Z0-9-_]+)/(?P<revision3_name>[a-zA-Z0-9-_]+)(/?)',
         'func': revision3,
     })
+
+def revision3_broadcast(self):
+    if 'id' not in self.data:
+        if not self.mainTree:
+            self.buildMainTree()
+        import re
+        prog = re.compile('^(.*)http://revision3.com/html5player-s(?P<id>\d+)')
+        for textarea in self.mainTree.iter('textarea'):
+            match = prog.match(textarea.text)
+            if match:
+                self.data['id'] = match.group('id')
+                self.saveData()
+                break
+    id = self.data['id']
+    width = 640
+    height = 360
+    if self.width:
+        width = self.width
+    if self.height:
+        height = self.height
+    return self.render('revision3_broadcast.html', {'id': id, 'width': width, 'height': height})
+
+services.append({
+        're': '^http(s?)://(.*)revision3.com/(?P<revision3_group>[a-zA-Z0-9-_]+)(/?)',
+        'func': revision3_broadcast,
+    })
